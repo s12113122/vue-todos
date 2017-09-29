f39e782c2f91b7293b8a99991890e314b0231528
s12113122 <s12113122@stu.edu.tw>
2017-09-29 17:45:30 +0800

    test1

---
+++ src/components/layouts.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container" > <!--最外层容器-->
+    <section class="menu"> <!--左边的容器-->
+    <menus></menus>
+    </section>
+    <section class="content-container"><!--右边的容器-->
+    <todo></todo>
+    </section>
+  </section>
+</template>
+<script>
+import todo from './todo';
+import menus from './menus';
+  export  default {
+    components:{
+      menus,
+      todo
+    }
+  };
+</script>
+<style lang="less">
+@import '../common/style/layouts.less';
+</style>
